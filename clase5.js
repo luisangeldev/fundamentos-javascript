74de50008477bee702ee6f692b0de94684ae5ff2
Luis Angel <soyluisbits@gmail.com>
2019-03-08 16:03:45 -0500

    Clase 9: Desestructurando objetos

--- clase5.js
+++ clase5.js
@@ -10,9 +10,15 @@ var dario = {
     edad: 27
 }
 
+/* // Desestructurando objeto desde parámetro
 function imprimirNombreEnMayusculas({nombre}) {
     console.log(nombre.toUpperCase());
-}
+} */
 
+// Desestructurando objeto en una sola línea
+function imprimirNombreEnMayusculas(persona) {
+    var {nombre} = persona; // <= var nombre = persona.nombre;
+    console.log(nombre.toUpperCase());
+}
 imprimirNombreEnMayusculas(luis);
 imprimirNombreEnMayusculas(dario);
