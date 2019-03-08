1eb5b41d0d6e7a3b390b93c834e9b17a2623fd59
Luis Angel <soyluisbits@gmail.com>
2019-03-08 14:03:35 -0500

    Agrega Clase 4: Funciones

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'Luis', edad = 28;
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Vicky', 28);
+imprimirEdad('Hector', 24);
+imprimirEdad('Dario', 23);
